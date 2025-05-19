39d40bd2b1c3f861c73570cdfe1a53e2dec5f29d
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2025-05-19 00:12:16 -0500

    fix lint errors, remove unused files

--- libs/shared-ui/src/index.ts
+++ libs/shared-ui/src/index.ts
@@ -1,5 +1,4 @@
+export { getQueryClient } from './lib/data-access/getQueryClient';
 export { trpc } from './lib/data-access/trpc';
-export { getQueryClient as getQueryClient } from './lib/hooks/getQueryClient';
-export { SharedUi } from './lib/shared-ui';
 export { isServer } from './lib/utils/utils';
 
--- libs/shared-ui/src/lib/shared-ui.tsx
+++
@@ -1,9 +0,0 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
-
-import styles from './shared-ui.scss?inline';
-
-export const SharedUi = component$(() => {
-  useStylesScoped$(styles);
-
-  return <>SharedUi works!</>;
-});
